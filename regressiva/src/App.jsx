2fd33dc191baf6b03f28dc91301230212b823db0
celoaraujo <celoparaujo99@gmail.com>
2024-05-09 16:47:13 -0300

    formulario da home page

--- regressiva/src/App.jsx
+++ regressiva/src/App.jsx
@@ -1,26 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Title from './components/Title'
-import Counter from './components/Counter'
-import Loading from "./assets/newyear.jpg"
+import { useState } from "react";
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
+
+import Loading from "./assets/newyear.jpg";
+
+
 
 function App() {
+  
 
   return (
     <>
-      <div className='app' style={{backgroundImage:`url(${Loading})`}}>
-       <div className="container">
-       <Title title="Contagem regressiva para 2023"/>
-       <div className="countdown-container">
-        <Counter title="Dias" number={2}/>
-        <Counter title="Horas" number={2}/>
-        <Counter title="Minutos" number={2}/>
-        <Counter title="Segundos" number={2}/>
-       </div>
-       </div>
+      <div className="app" style={{ backgroundImage: `url(${Loading})` }}>
+        <div className="container">
+          <Outlet/>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
