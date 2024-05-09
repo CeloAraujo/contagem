a5f4109136a001554cbfa95abfeff90f6fd5b0b7
celoaraujo <celoparaujo99@gmail.com>
2024-05-09 16:58:02 -0300

    useState- reunindo dados do form

--- regressiva/src/routes/Home.jsx
+++ regressiva/src/routes/Home.jsx
@@ -1,16 +1,37 @@
-import './Home.css'
+import "./Home.css";
+
+import { useState } from "react";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color,
+    };
+    console.log(eventObject);
+  };
+
   return (
     <div className="home">
       <h2>Monte sua contagem regressiva!</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
           <input
             type="text"
             name="title"
             placeholder="Digite o título do evento"
+            onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </label>
         <label>
@@ -18,6 +39,8 @@ const Home = () => {
           <input
             type="date"
             name="date"
+            onChange={(e) => setDate(e.target.value)}
+            required
           />
         </label>
         <label>
@@ -26,6 +49,7 @@ const Home = () => {
             type="text"
             name="image"
             placeholder="Insira a URL da imagem"
+            onChange={(e) => setImage(e.target.value)}
           />
         </label>
         <label>
@@ -33,6 +57,8 @@ const Home = () => {
           <input
             type="color"
             name="color"
+            onChange={(e) => setColor(e.target.value)}
+            required
           />
         </label>
         <input type="submit" value="Enviar" />
