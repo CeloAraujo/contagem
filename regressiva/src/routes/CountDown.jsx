cda1443985f3245a37302463d2ed0501bbae7ca4
celoaraujo <celoparaujo99@gmail.com>
2024-05-10 15:09:43 -0300

    uso do contexto

--- regressiva/src/routes/CountDown.jsx
+++ regressiva/src/routes/CountDown.jsx
@@ -2,11 +2,23 @@ import Title from "../components/Title";
 import Counter from "../components/Counter";
 import useCountdown from "../hooks/useCounteDown";
 
+import { CountDownContext } from "../context/CountDownContext";
+import { useContext } from "react";
+
+import { Navigate } from "react-router-dom";
+
 const CountDown = () => {
-    const [day, hour, minute, second] = useCountdown("Jan 1, 2025 00:00:00");
+  const { event } = useContext(CountDownContext);
+
+  if (!event) return <Navigate to="/" />;
+
+  const eventTitle = event.title;
+  const eventColor = event.color;
+
+  const [day, hour, minute, second] = useCountdown(event.date);
   return (
     <>
-      <Title title="Contagem regressiva para 2025" />
+      <Title title={eventTitle} />
       <div className="countdown-container">
         <Counter title="Dias" number={day} />
         <Counter title="Horas" number={hour} />
